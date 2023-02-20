43b91e62feb460ab693a5d9f35f2ff14edd7bb8d
wbsserious <125722460+wbsserious@users.noreply.github.com>
2023-02-21 01:14:24 +0800

    Update 2349.py

--- 2349.py
+++ 2349.py
@@ -1,5 +1,14 @@
 class NumberContainers(object):
+# 设计一个数字容器系统，可以实现以下功能：
 
+# 在系统中给定下标处 插入 或者 替换 一个数字。
+# 返回 系统中给定数字的最小下标。
+# 请你实现一个 NumberContainers 类：
+
+# NumberContainers() 初始化数字容器系统。
+# void change(int index, int number) 在下标 index 处填入 number 。如果该下标 index 处已经有数字了，那么用 number 替换该数字。
+# int find(int number) 返回给定数字 number 在系统中的最小下标。如果系统中没有 number ，那么返回 -1 。
+ 
     def __init__(self):
         self.table={}
         self.tableindex={}
