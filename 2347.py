0228252768ee16a47b49fe268304a82319458c85
wbsserious <125722460+wbsserious@users.noreply.github.com>
2023-02-21 01:13:28 +0800

    Update 2347.py

--- 2347.py
+++ 2347.py
@@ -1,5 +1,13 @@
 def bestHand(ranks, suits):
-    #   """
+#     #   ""给你一个整数数组 ranks 和一个字符数组 suit 。你有 5 张扑克牌，第 i 张牌大小为 ranks[i] ，花色为 suits[i] 。
+
+# 下述是从好到坏你可能持有的 手牌类型 ：
+
+# "Flush"：同花，五张相同花色的扑克牌。
+# "Three of a Kind"：三条，有 3 张大小相同的扑克牌。
+# "Pair"：对子，两张大小一样的扑克牌。
+# "High Card"：高牌，五张大小互不相同的扑克牌。
+# 请你返回一个字符串，表示给定的 5 张牌中，你能组成的 最好手牌类型 。"
     #     :type ranks: List[int]
     #     :type suits: List[str]
     #     :rtype: str
